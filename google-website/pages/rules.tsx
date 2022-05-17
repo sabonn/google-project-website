fde66752332a22c947aa4ab10a18f08a637e29b6
sabonn <itay.eshkar@gmail.com>
2022-05-17 19:06:07 +0300

    adding the rules to the website

--- google-website/pages/rules.tsx
+++ google-website/pages/rules.tsx
@@ -11,6 +11,7 @@ const rules = () => {
                 <h2 className={styles.title}>
                     The Rules
                 </h2>
+                <p className={styles.rules}>There are two groups hiders and seekers every player has a map on hes screen which tells his location and maybe the location of his teammates location the seekers also get a hot an cold meter that tells them of their close to any of the hiders the goal of the hiders is to not get caught by the seekers for as long as possible and there is the point system which allows players to get attributes or sbotage their aponants in cost of points after the end of a round Point determined who won after the lobby ended</p>
             </main>
         </div>
     );
