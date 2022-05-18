d775cc137d68299f3899fc06f27ba5223333950c
sabonn <itay.eshkar@gmail.com>
2022-05-18 19:53:40 +0300

    adding an input section

--- google-website/pages/create_game.tsx
+++ google-website/pages/create_game.tsx
@@ -9,6 +9,15 @@ const create_game = () => {
             <a href='./' className={styles.go_back}>Go Back</a>
             <main className={styles.main}>
                 <h2 className={styles.title}>Create A Game</h2>
+                <form action="/api/form" method="post" className={styles.grid}>
+                    <label htmlFor="first">First Name</label>
+                    <input className={styles.info} type="text" id="first" name="first" required />
+
+                    <label htmlFor="last">Last Name</label>
+                    <input className={styles.info} type="text" id="last" name="last" required />
+                
+                    <button>&rarr;</button>
+                </form>
             </main>
         </div>
     );
--- google-website/pages/join_game.tsx
+++ google-website/pages/join_game.tsx
@@ -9,6 +9,15 @@ const join_game = () => {
             <a href='./' className={styles.go_back}>Go Back</a>
             <main className={styles.main}>
                 <h2 className={styles.title}>Join A Game</h2>
+                <form action="/api/form" method="post">
+                    <label htmlFor="first">First Name</label>
+                    <input className={styles.info} type="text" id="first" name="first" required />
+
+                    <label htmlFor="last">Last Name</label>
+                    <input className={styles.info} type="text" id="last" name="last" required />
+                    
+                    <button>&rarr;</button>
+                </form>
             </main>
         </div>
     );
