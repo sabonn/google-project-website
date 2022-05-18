4aab641ec97855b4b7b6ddfef04be511cf7f9e6c
sabonn <itay.eshkar@gmail.com>
2022-05-18 21:49:11 +0300

    repositening the input fields and addj g

--- google-website/pages/create_game.tsx
+++ google-website/pages/create_game.tsx
@@ -6,18 +6,19 @@ const {useState} = React;
 const create_game = () => {
     return (
         <div className={styles.container}>
+            <script type = 'javascript' src='./index.js'></script>
             <a href='./' className={styles.go_back}>Go Back</a>
             <main className={styles.main}>
                 <h2 className={styles.title}>Create A Game</h2>
-                <form action="/api/form" method="post" className={styles.grid}>
-                    <label htmlFor="first">First Name</label>
-                    <input className={styles.info} type="text" id="first" name="first" required />
-
-                    <label htmlFor="last">Last Name</label>
-                    <input className={styles.info} type="text" id="last" name="last" required />
                 
-                    <button>&rarr;</button>
-                </form>
+                <label htmlFor="username">username</label>
+                <input className={styles.info} type="text" id="username" name="username" required />
+
+                <label htmlFor="field">field size</label>
+                <input className={styles.info} type="text" id="field" name="field" required />
+                
+                <button onClick={() => window.alert("yes")}>&rarr;</button>
+
             </main>
         </div>
     );
--- google-website/pages/join_game.tsx
+++ google-website/pages/join_game.tsx
@@ -9,15 +9,14 @@ const join_game = () => {
             <a href='./' className={styles.go_back}>Go Back</a>
             <main className={styles.main}>
                 <h2 className={styles.title}>Join A Game</h2>
-                <form action="/api/form" method="post">
-                    <label htmlFor="first">First Name</label>
-                    <input className={styles.info} type="text" id="first" name="first" required />
 
-                    <label htmlFor="last">Last Name</label>
-                    <input className={styles.info} type="text" id="last" name="last" required />
+                <label htmlFor="token">token</label>
+                <input className={styles.info} type="text" id="token" name="token" required />
+
+                <label htmlFor="username">username</label>
+                <input className={styles.info} type="text" id="username" name="username" required />
                     
-                    <button>&rarr;</button>
-                </form>
+                <button onClick={() => window.alert("yes")}>&rarr;</button>
             </main>
         </div>
     );
