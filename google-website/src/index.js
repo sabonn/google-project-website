ba6461809d74c9263c19504d36001cd225ce082c
sabonn <itay.eshkar@gmail.com>
2022-05-24 20:08:39 +0300

    making an nfc function

--- google-website/src/index.js
+++ google-website/src/index.js
@@ -38,6 +38,28 @@ const join = () => {
     }
 }
 
+const nfc = async () => {
+    log("User clicked scan button");
+  
+    try {
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      log("> Scan started");
+
+      ndef.addEventListener("readingerror", () => {
+        log("Argh! Cannot read data from the NFC tag. Try another one?");
+      });
+
+      ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        log(`> Serial Number: ${serialNumber}`);
+        log(`> Records: (${message.records.length})`);
+      });
+    } catch (error) {
+      log("Argh! " + error);
+  }
+}
+
+/*
 scanButton.addEventListener("click", async () => {
     log("User clicked scan button");
   
@@ -58,7 +80,7 @@ scanButton.addEventListener("click", async () => {
       log("Argh! " + error);
     }
   });
-/*
+
 writeButton.addEventListener("click", async () => {
     log("User clicked write button");
   
