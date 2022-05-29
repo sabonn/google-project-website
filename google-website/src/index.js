efb35e3f661a0bb6bf41651e9022a4c839637c38
sabonn <itay.eshkar@gmail.com>
2022-05-29 16:14:17 +0300

    adding some shit

--- google-website/src/index.js
+++ google-website/src/index.js
@@ -1,28 +1,3 @@
-import { help } from 'module';
-import { initializeApp } from '../node_modules/firebase/app';
-import { getFireStore, doc, setDoc, getDoc, updateDoc} from '../node_modules/firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCXw_0YXnov8iTar6jJF0j-NbOPysSRmAY",
-  authDomain: "website-1e26f.firebaseapp.com",
-  projectId: "website-1e26f",
-  storageBucket: "website-1e26f.appspot.com",
-  messagingSenderId: "1036462298481",
-  appId: "1:1036462298481:web:d72949366b7b5e0858d2d0",
-  measurementId: "G-D1E324ZDFP"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFireStore(app);
-const test = doc(firestore, 'data/AGhBvGUIJhB8OZxOMM2Q');
-
-const write_db = () => {
-  const docData = {
-    players: ["yes", "no"]
-  }
-  updateDoc(test, docData);
-}
-
 var api_token ="AIzaSyCEY6YMFUn3rzCTPO_ZA1gX40WQaO6FkPE";
 var votes = 0, is_pressed = false, token_game = 0;
 
@@ -33,6 +8,34 @@ const create_token = () => {
     }
     return temp;
 }
+
+const get_location = () => {
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    window.alert("geolocation is not supported in this browser");
+  }
+}
+
+const showPosition = (position) => {
+  alert("Laltitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude);
+}
+
+joining.addEventListener('click', () => {
+  var name = document.getElementById('username').value;
+  var size = document.getElementById('field').value;
+  if(name != "" && size != ""){
+    fetch('../data/index.json')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        data.games.push({game:{players:[name.toString()], votes:0, start:false}});
+        window.alert("token: " + data.games.length.toString());
+      });
+  } else {
+    window.alert("check that everything is full");
+  }
+});
 
 create.addEventListener('click', () => {
     var name = document.getElementById('username').value;
@@ -48,55 +51,6 @@ create.addEventListener('click', () => {
     } else {
       window.alert("check that everything is full");
     }
-});
-
-const check_start = () => {
-  fetch('../data/index.json')
-    .then(result => result.json())
-    .then(data => {
-      if(data[123].start){
-        window.location.href = './game.html';
-      }
-    });
-}
-
-vote.addEventListener('click' ,() => {
-  if(!is_pressed){
-    votes++;
-    is_pressed = true;
-  } else {
-    window.alert("you have all ready voted");
-  }
-
-  fetch('../data/index.json')
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      document.getElementById('votes').innerHTML = "votes: " + votes.toString();
-      data[token].vote = votes;
-      if(data[token].vote > data[token].players.length){
-        data[token].start = true;
-      }
-    });
-});
-
-join.addEventListener('click', () => {
-    var name = document.getElementById('userjoin').value;
-    var token = document.getElementById('token').value;
-    fetch('../data/index.json')
-      .then(responce => responce.json())
-      .then(data => {
-        console.log(data);
-        if (token == ""){
-          window.alert("please enter a token");
-        } else if(parseInt(token) <= data.games.length && !data[parseInt(token)].players.includes(name.toString())){
-          token_game = parseInt(token);
-          data[token_game].players.push(name.toString());
-          window.location.href = './waiting.html';
-        } else {
-          window.alert("check that everything is valid");
-        }
-    });
 });
 
 /*nfc functions
