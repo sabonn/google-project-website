fd0947031c5b7e7f22d0974fa9e67db784eb52b4
sabonn <itay.eshkar@gmail.com>
2022-05-30 15:46:40 +0300

    adding the google maps code to desplay location and adding <html> to my html files

--- google-website/src/index.js
+++ google-website/src/index.js
@@ -1,9 +1,22 @@
-var api_token ="AIzaSyCEY6YMFUn3rzCTPO_ZA1gX40WQaO6FkPE";
 var votes = 0, is_pressed = false, token_game = 0;
 
 var data = {
   games:[]
 }
+
+const initMap = () => {
+  const uluru = { lat: -25.344, lng: 131.031 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
 
 const create_token = () => {
     var temp = "";
