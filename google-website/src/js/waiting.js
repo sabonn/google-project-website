62c3e35cc9af2fc84ecd489298bd1ccfb8241554
kevin1467 <69971862+kevin1467@users.noreply.github.com>
2022-06-09 17:09:27 +0300

    OMG I DID NOT MEAN TO DELETE THAT
    this reverses a part of my last commit

--- google-website/src/js/waiting.js
+++ google-website/src/js/waiting.js
@@ -1,7 +1,16 @@
-import fetch from "node-fetch";
+var index = 0, game_index = 0;
 
-fetch("./voting.json")
-.then(response => {
-   return response.json();
-})
-.then(jsondata => console.log(jsondata));
+voting.addEventListener('click', () => {
+    fetch('http://10.78.131.31:9999/data_changes')
+        .then(result => result.json())
+        .then(data => {
+            console.log(data);
+            window.location = './game.html';
+            if(!data.games[game_index].pressed[index]){
+                data.games[game_index].vote++;
+                data.games[game_index].pressed[index] = true;
+            }
+            if(data.games.vote > data.games.length) data.games.start = true;
+            if(data.games.start) window.location = './game.html';
+    });
+});
