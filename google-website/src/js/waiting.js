697e94c0eb23952eea35208f5d05ce089e86ae15
kevin1467 <69971862+kevin1467@users.noreply.github.com>
2022-06-09 17:07:11 +0300

    this lets you send inforrmation to the sevrer
    i am not a graphic desinger just forget about how dumb it looks, WE HAVE POST FROM THE WEBSTIE

--- google-website/src/js/waiting.js
+++ google-website/src/js/waiting.js
@@ -1,16 +1,7 @@
-var index = 0, game_index = 0;
+import fetch from "node-fetch";
 
-voting.addEventListener('click', () => {
-    fetch('http://10.78.131.31:9999/data_changes')
-        .then(result => result.json())
-        .then(data => {
-            console.log(data);
-            window.location = './game.html';
-            if(!data.games[game_index].pressed[index]){
-                data.games[game_index].vote++;
-                data.games[game_index].pressed[index] = true;
-            }
-            if(data.games.vote > data.games.length) data.games.start = true;
-            if(data.games.start) window.location = './game.html';
-    });
-});
+fetch("./voting.json")
+.then(response => {
+   return response.json();
+})
+.then(jsondata => console.log(jsondata));
