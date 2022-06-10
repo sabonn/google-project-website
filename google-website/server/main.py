81aa27c1a629942a4b76fa0919b6a6f2f751da94
sabonn <itay.eshkar@gmail.com>
2022-06-10 11:39:10 +0300

    cleaning files and code

---
+++ google-website/server/main.py
@@ -0,0 +1,44 @@
+from flask import Flask, redirect, url_for, request
+import socket
+import json
+hostName = socket.gethostname()
+hostName = socket.gethostbyname(hostName)
+t = []
+output = ''
+obj = {'games': []}
+output += '<form method="POST">'
+output += '<input name="d" type="text" placeholder="data">'
+output += '<input type="submit" value="enter1">'
+
+server = Flask(__name__)
+
+@server.route('/', methods = ['POST', 'GET'])
+def ma():
+    global obj
+    if request.method == 'POST':
+       data = request.get_data()
+       data.decode()
+       obj = json.loads(data)
+       print(obj)
+
+       return redirect(url_for('ma'))
+    elif request.method == 'GET':
+        return output
+
+
+
+@server.route('/data_change', methods=['GET', 'POST'])
+def data_handler():
+    global obj
+    if request.method == 'GET':
+        print(obj)
+        print('p')
+        return obj
+
+
+    if request.method == 'POST':
+        return redirect("http://", code=302)
+
+
+
+server.run(port=9999, host=hostName)
--- google-website/src/js/index.js
+++ google-website/src/js/index.js
@@ -1,31 +1,70 @@
-var votes = 0, is_pressed = false, token_game = 0, username = "";
+var angle = 90, game_index = 0, index = 0;
 
-const create_token = () => {
-    var temp = "";
-    for(var i = 0; i < 18;i++){
-        temp += (Math.round(Math.random()*9) + 1).toString();
+document.getElementById('arrow').style.transform = 'rotate(' + angle.toString() +'deg)';
+
+fetch('http://10.78.131.31:9999/data_changes')
+  .then(result => result.json())
+  .then(data => {
+    console.log(data);
+  });
+
+const get_location = () => {
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      window.alert("geolocation is not supported in this browser");
     }
-    return temp;
+  }
+  
+  const showPosition = (position) => {
+    alert("Laltitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude);
+  }
+
+/*nfc functions
+const nfc = async () => {
+    log("User clicked scan button");
+  
+    try {
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      log("> Scan started");
+
+      ndef.addEventListener("readingerror", () => {
+        log("Argh! Cannot read data from the NFC tag. Try another one?");
+      });
+
+      ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        log(`> Serial Number: ${serialNumber}`);
+        log(`> Records: (${message.records.length})`);
+      });
+    } catch (error) {
+      log("Argh! " + error);
+  }
+}
+if(window.location == './waiting.html'){
+  check_start();
 }
 
-create.addEventListener('click', () => {
-  var name = document.getElementById('username').value;
-  var size = document.getElementById('field').value;
-  fetch('http://10.78.131.31:9999/data_changes')
-    .then(result => result.json())
-    .then(data => {
-      console.log(data);
-      if(name != "" && size != ""){
-        username = name.toString();
-        localStorage.setItem("username", username);
-        window.location.href = "./waiting.html";
-        data.games.push({players:[], votes: 0, start:false});
-      } else {
-        window.alert("check that everything is full");
-      }
-    });
+const write = async () => {
+    log("User clicked write button");
+  
+    try {
+      const ndef = new NDEFReader();
+      await ndef.write("Hello world!");
+      log("> Message written");
+    } catch (error) {
+      log("Argh! " + error);
+    }
 });
-
-const get_username = () => {
-  return localStorage.getItem("username");
-}
+  
+cosnt read_only = async () => {
+    log("User clicked make read-only button");
+  
+    try {
+      const ndef = new NDEFReader();
+      await ndef.makeReadOnly();
+      log("> NFC tag has been made permanently read-only");
+    } catch (error) {
+      log("Argh! " + error);
+    }
+});*/
